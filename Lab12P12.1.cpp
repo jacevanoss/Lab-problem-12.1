c5cea3d7dbb61c839ad1e0defc39d7844102fa7b
Jace Van Oss <j-vanoss@onu.edu>
2024-11-21 10:13:46 -0500

    completed P12-1

--- Lab12P12.1.cpp
+++ Lab12P12.1.cpp
@@ -10,6 +10,26 @@ int main() {
 	string datafile;
 	cout << "input file: ";
 	getline(cin, datafile);
+	ifstream inputFile;
+	string word;
+	int wordCount = 0;
+	int charCount = 0;
+	inputFile.open(datafile);
+	if (!inputFile) {
+		cout << "Error opening file\n";
+		return 1;
+	}
+	while (inputFile >> word) {
+		wordCount++;
+		for (int i = 0; i < word.length(); i++) {
+			if (word[i] != ' ' && word[i] != '\n' && word[i] != '\t') {
+				charCount++;
+			}
+		}
+	}
+	cout << "Words: " << wordCount << endl;
+	cout << "Characters: " << charCount << endl;
+
 	pressEnterToContinue();
 
 	return 0; 
