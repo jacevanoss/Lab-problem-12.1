23c87631f31da0f97da97fff2e14b0810a5d76d9
Jace Van Oss <j-vanoss@onu.edu>
2024-11-21 09:53:53 -0500

    completed pressEnterToContinue

--- Lab12P12.1.cpp
+++ Lab12P12.1.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
+void pressEnterToContinue();
+
 int main() {
+	string datafile;
+	cout << "input file: ";
+	getline(cin, datafile);
+	pressEnterToContinue();
 
 	return 0; 
+}
+void pressEnterToContinue() {
+	cout << "\npress enter to continue...\n";
+	cin.clear();
+	while (cin.get() != '\n') continue;
 }
